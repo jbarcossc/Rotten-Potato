cdd4083eef5952268ceedfd01130f71aadadf157
jbarcossc <juancruzbarcos@hotmail.com>
2023-12-26 10:24:53 -0300

    navbar setup

--- rotten-potato/src/App.js
+++ rotten-potato/src/App.js
@@ -1,9 +1,14 @@
-import './App.css';
+import './App.scss';
+import Footer from './components/Footer.js';
+import Header from './components/Header.js';
+import Wrapper from './components/Wrapper.js';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header></Header>
+      <Wrapper></Wrapper>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ rotten-potato/src/components/Wrapper.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './../styles/Wrapper.scss'
+
+export default class Wrapper extends Component {
+  render() {
+    return (
+      <main id="wrapper">
+        Wrapper
+      </main>
+    )
+  }
+}
